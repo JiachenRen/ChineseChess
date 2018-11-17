814967dafa0710093cc4d57692548c930a9378aa
jiachenren <arc4jiachenren@gmail.com>
2018-11-17 14:40:24 -0500

    Initial Commit

---
+++ ChineseChess/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ChineseChess
+//
+//  Created by Jiachen Ren on 11/17/18.
+//  Copyright © 2018 Jiachen Ren. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ChineseChess/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ChineseChess
+//
+//  Created by Jiachen Ren on 11/17/18.
+//  Copyright © 2018 Jiachen Ren. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
